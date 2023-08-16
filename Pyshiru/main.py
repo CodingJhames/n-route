c97f7c08671f68297c910d424ecc009d7a809b8c
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-08-16 17:15:54 -0500

    instance variables

---
+++ Pyshiru/main.py
@@ -0,0 +1,9 @@
+
+
+class Persona():
+  def __init__(self,nombre, nacionalidad ):
+      self.nombre = nombre
+      self.nacionalidad = nacionalidad
+
+persona_one = Persona('James','Colombiano')
+print(persona_one)
