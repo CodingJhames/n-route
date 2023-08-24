cdb1373b637ac1daabe986d632b687003ffeb5e5
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-08-24 13:13:22 -0500

    arrays added

---
+++ OOP/poo/src/main/java/Arreglos/EjemArrays.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Arreglos;
+
+/**
+ *
+ * @author james
+ */
+public class EjemArrays {
+    
+    
+    
+    
+    public static void main(String[] args) {
+        int array[] = new int[3];
+        array[0] = 25;
+        array[1] = 20;
+        array[2] = 30;
+        
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    
+}
