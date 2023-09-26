02030fbe86425109a24071a567e209f61764a064
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-09-26 18:44:48 -0500

    intro complete

--- patterns/intro/js/intro.js
+++ patterns/intro/js/intro.js
@@ -20,3 +20,77 @@ function operation(fn, a, b){
 }
 
 operation(sum, 10,20);
+
+//  arrow function
+
+let fA = () => console.log("algo");
+
+fA();
+
+operation((a,b)=>a*b, 5,5);
+operation((a,b)=>{
+    const c = a + b;
+    return c * 2;
+},1,2);
+
+// foreach
+const names = ["Héctor","Juan","Pablo"];
+names.forEach((name)=>console.log(name));
+names.forEach((name)=>console.log(name.toUpperCase()));
+console.log(names);
+
+// map
+const namesUpper = names.map((name)=>name.toUpperCase());
+console.log(namesUpper);
+console.log(names);
+
+// reduce
+const numbers = [5,4,7,1,10];
+const total = numbers.reduce((ac, number)=>ac + number, 0);
+console.log(total);
+
+
+// Programación ORIENTADA A OBJETOS
+// clase
+class Drink{
+    constructor(name){
+        this.name = name;
+    }
+
+    info(){
+        return "la bebida es: " + this.name;
+    }
+}
+
+// objetos con funciones
+function Drink2(name){
+    this.name = name;
+    this.info = function(){
+        return "la bebida es: " + this.name;
+    }
+}
+
+
+const drink = new Drink("agua");
+console.log(drink.info());
+
+const drink2 = new Drink2("agua 2");
+console.log(drink2.info());
+
+// herencia
+class Beer extends Drink{
+
+    constructor(name, alcohol){
+        super(name);
+        this.alcohol = alcohol;
+    }
+
+    info(){
+        return super.info() + " "+this.alcohol;
+    }
+}
+
+const beer = new Beer("erdinger", 8.5);
+console.log(beer.info());
+
+
